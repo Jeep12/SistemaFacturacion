f6eb59b16170fac91e3f8ebb3126885e68b3cfd5
Juan Encabo <encabojuan@gmail.com>
2023-07-03 18:13:53 -0300

    agrego boton para reenviar correo de verificacion, falta proteccion

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-verify-email',
@@ -7,15 +8,40 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./verify-email.component.css']
 })
 export class VerifyEmailComponent implements OnInit {
-  email:string | any ;
-  constructor(private afAuth:AngularFireAuth) { }
+  email: string | any;
+  isButtonDisabled = false;
+
+  constructor(private afAuth: AngularFireAuth, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.afAuth.authState.subscribe(user=>{
+    this.afAuth.authState.subscribe(user => {
       console.log(user)
-      this.email=  user?.email;
+      this.email = user?.email;
 
-    })
+    });
+    this.disableButtonForSeconds(5); // Deshabilitar el botón durante 5 segundos después de iniciar el componente
+
   }
 
+  resendVerificationEmail() {
+    if (this.isButtonDisabled) {
+      return; // Evitar que se haga clic en el botón mientras está deshabilitado
+    }
+    this.afAuth.currentUser.then(user => {
+      user?.sendEmailVerification().then(response => {
+        this.toastr.success('Correo de verificación enviado correctamente');
+
+      }).catch(error => {
+        this.toastr.error('Error al enviar el correo de verificación', error.message);
+
+      }).finally(() => {
+        this.disableButtonForSeconds(10); // Volver a habilitar el botón después de 10 segundos
+      });
+    })
+  }
+  private disableButtonForSeconds(seconds: number) {
+    setTimeout(() => {
+      this.isButtonDisabled = false; // Habilitar el botón después del tiempo especificado
+    }, seconds * 1000);
+  }
 }
